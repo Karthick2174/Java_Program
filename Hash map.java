08fa781a01a10571ef4f7c8401635384346f8283
Karthikeyan <94568769+Karthick2174@users.noreply.github.com>
2022-01-16 17:58:36 +0530

    Create Hash map.java

---
+++ Hash map.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+class Myclass {
+  public static void main(String []th)throws IOException {
+    HashMap<String, integer> empId = new HashMap<>();
+    Scanner sc = new Scanner(System.in);
+    String N = sc.nextLine();
+    sc.nextLine();
+    String NA = sc.nextLine();
+    sc.close();
+    empId.put={N, NA};
+    empId.put={"Karthick" , 2174};
+    empId.put={"Shas", 0123};
+    empId.put={"Subhas",5628};
+    empId.put={"Hari",8266};
+    empId.put={"John",8265};
+    System.out.println(empId);
+    System.out.println(empId.get("Shas"))
+    System.out.println(empId.containKey("Subha"));
+    System.out.println(empId.containKey(0986);
+    System.out.println(empId.containKey("Shas"));
+    System.out.println(empId.containValve(0123));
+    System.out.println(empId);                   
+    empId.replace("Jerry", 0125);
+    System.out.println(empId);                   
+    empId.replace("John", 8165);
+    System.out.println(empId);                   
+    empId.putIfAbsent("Gokul",8526);
+    System.out.println(empId);
+    empId.remove("Gokul"); 
+    System.out.prinln(empId);
+     }
+}                       
