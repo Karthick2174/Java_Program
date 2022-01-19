ff2e0965007e0cce2ea0eb55ca93e16cd113abd4
Karthikeyan <94568769+Karthick2174@users.noreply.github.com>
2022-01-19 23:40:14 +0530

    Create marks.java

---
+++ marks.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+import java.lang.*;
+class Myclass {
+  public static void main(String [] args)throws IOException
+  {
+    int d, t, y, m, sum;
+    Scanner scan = new Scanner(System.in);
+    System.out.pintln("Entre a number of subject : ");
+    d= scan.nextInt();
+    System.out.println("Entre a name of subject : ";
+    
